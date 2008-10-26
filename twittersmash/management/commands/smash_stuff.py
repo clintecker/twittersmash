b1d6998e3ee904237ee3c5730d9bce7de16eb64e
Clinton Ecker <clint@arstechnica.com>
2008-10-26 01:51:54 -0500

    Beefing up tests

--- twittersmash/management/commands/smash_stuff.py
+++ twittersmash/management/commands/smash_stuff.py
@@ -4,6 +4,7 @@ import feedparser
 import re
 import string
 from django.core.management.base import BaseCommand
+from optparse import make_option
 from twittersmash.models import Feed, TwitterAccount, Message
 
 import pytz
@@ -16,30 +17,51 @@ twit_re = re.compile(r'^(?P<username>\S+): (?P<message>.*)$')
 
 class Command(BaseCommand):
     help = "Loops through feeds and determines if messages need to be sent to any twitter accounts"
+    option_list = BaseCommand.option_list + (
+        make_option('--dryrun', '-d', action='store_true', dest='dryrun', default=False,
+            help='Go through the motions but commit nothing to Twitter'),
+        make_option('--quiet', '-q', action='store_true', dest='quiet', default=False,
+            help='Don\t print anything to console'),
+    )
 
     def handle(self, *args, **options):
         # Get list of TwitterAccounts
+        quiet = options.get('quiet')
+        entries_pulled = 0
+        accounts_skipped = 0
+        accounts_ready = 0
+        entries_tweeted = 0
+        feeds_pulled = 0
+        messages_added = 0
+        feeds_checked = 0
         accounts = TwitterAccount.objects.all().filter(active=True)
         for account in accounts:
             keywords = account.philter.lower().split(',')
             keywords = map(string.strip, keywords)
             api = twitter.Api(username=account.username, password=account.password)
-            print "Checking %s" % (account,)
+            if not quiet:
+                print "Checking %s" % (account,)
             feed_list = account.feeds.all()
             for f in feed_list:
-                print " - %s" % (f,)
+                feeds_checked += 1
+                if not quiet:
+                    print " - %s" % (f,)
                 # Get list of feeds who'se last_update + polling_rate is less than now
                 if f.last_checked == None or f.last_checked + \
                 datetime.timedelta(minutes=f.polling_rate) < datetime.datetime.now():
+                    accounts_ready += 1
                     # Update timestamp
                     f.last_checked = datetime.datetime.now()
                     f.save()
-                    print "   * Pulling feed"
+                    if not quiet:
+                        print "   * Pulling feed"
                     # Pull each feed
                     d = feedparser.parse(f.url)
+                    feeds_pulled += 1
                     # Loop through feed
                     d.entries.reverse()
                     for entry in d['entries']:
+                        entries_pulled += 1
                         guid = entry.id
                         tweeted = entry.updated_parsed
                         if twit_re.search(entry.title):
@@ -56,7 +78,7 @@ class Command(BaseCommand):
                             tweeted[5], 
                             tzinfo=None
                         )
-                        tweeted_dt_cst = central.localize(tweeted_dt_cst)
+                        tweeted_dt_cst = central.localize(tweeted_dt)
                         tweeted_dt_utc = tweeted_dt_cst.astimezone(utc)
                         
                         msg, created = Message.objects.get_or_create(
@@ -70,22 +92,36 @@ class Command(BaseCommand):
                         })
                         
                         if created:
+                            messages_added += 1
                             # Wasn't already in the db
                             for keyword in keywords:
                                 if keyword in message.lower():
                                     # We need to send it to the twitter account
                                     try:
-                                        status = api.PostUpdate(message)
+                                        if not options.get('dryrun'):
+                                            status = api.PostUpdate(message)
+                                            if not quiet:
+                                                print "   * Sent to Twitter (%s)" % (keyword,)
+                                        else:
+                                            if not quiet:
+                                                print "   * Dry run (%s)" % (keyword,)
+                                        entries_tweeted += 1
                                         msg.sent_to_twitter = True
                                         msg.save()
-                                        print "   * Sent to Twitter (%s)" % (keyword,)
                                     except e:
-                                        print "   - Failed to send to twitter (%s)" % (e,)
+                                        if not quiet:
+                                            print "   - Failed to send to twitter (%s)" % (e,)
                 else:
-                    print "   * Checked within the last %s minutes" % (f.polling_rate)  
-                    # If twitter account has a filter check each item for the filter text
-                        # If the message isn't in the Messages table, add it
-                            # Send message to twitter account
-                    # If the message isnt in the Messages table, add it
-                        # Send message to twitter account
-        
+                    if not quiet:
+                        print "   * Checked within the last %s minutes" % (f.polling_rate)
+                    accounts_skipped += 1
+        return {
+            'entries_pulled': entries_pulled,
+            'accounts_skipped': accounts_skipped,
+            'accounts_ready': accounts_ready,
+            'entries_tweeted': entries_tweeted,
+            'feeds_pulled': feeds_pulled,
+            'messages_added': messages_added,
+            'feeds_checked': feeds_checked,
+        }
+            
--- twittersmash/tests.py
+++ twittersmash/tests.py
@@ -25,16 +25,19 @@ class SmashTest(TestCase):
             philter = '',
             active = True,
         )
-        
-        #self.api = twitter.Api(username = self.ta1.username, password=self.ta1.password)
 
     def tearDown(self):
         pass
 
-    def testBasicSmash(self):
-        "Takes one feed and pipes it into one Twitter Account"
-        from twittersmash.management.commands.smash_stuff import Command as SmashStuff
-        # Add one feed to one TwitterAccount
+    def testTwitterConnection(self):
+        # Test twitter connection
+        self.api = twitter.Api(username = self.ta1.username, password=self.ta1.password)
+        
+    def testBasicSmashNoFilterNoTwitter(self):
+        "Test basic model functionality"
+        self.ta1.philter = ''
+        self.ta1.save()
+        
         self.ta1.feeds.add(self.feed1)
         self.message1 = Message.objects.create(
             message = 'This is a test message',
@@ -44,4 +47,189 @@ class SmashTest(TestCase):
             twitter_account = self.ta1,
             feed = self.feed1,
         )
+
+    def testBasicSmashFilterNoTwitter(self):
+        "Smash two feeds into one account, with a filter, no actual twittering"
+        self.ta1.philter = '#pdc'
+        self.ta1.save()
         
+        self.ta1.feeds.add(self.feed1)
+        self.ta1.feeds.add(self.feed2)
+        
+        self.message1 = Message.objects.create(
+            message = 'This is a test message',
+            guid = 'http://twitter.com/clint/status/975241573',
+            sent_to_twitter = True,
+            tweeted = datetime.datetime.now(),
+            twitter_account = self.ta1,
+            feed = self.feed1,
+        )
+        self.message2 = Message.objects.create(
+            message = 'I am Ken Fisher and I have approved this mesage',
+            guid = 'http://twitter.com/kevinrose/status/975842840',
+            sent_to_twitter = False,
+            tweeted = datetime.datetime.now(),
+            twitter_account = self.ta1,
+            feed = self.feed2,
+        )
+        self.message3 = Message.objects.create(
+            message = 'checked in and got a window seat for my red-eye flight to Newark tomorrow night.',
+            guid = 'http://twitter.com/Joi/status/975833289',
+            sent_to_twitter = True,
+            tweeted = datetime.datetime.now(),
+            twitter_account = self.ta1,
+            feed = self.feed2,
+        )
+        self.message4 = Message.objects.create(
+            message = 'I\'m at Schaumburg, IL (Schaumburg, IL, USA) in Schaumburg - http://bkite.com/021de',
+            guid = 'http://twitter.com/Schwieb/status/975825768',
+            sent_to_twitter = True,
+            tweeted = datetime.datetime.now(),
+            twitter_account = self.ta1,
+            feed = self.feed1,
+        )
+
+    def testSmashStuffCommandNoFilter(self):
+        "Sets up two feeds to get smashed into another and runs through the smash_stuff command with no filter"
+        from twittersmash.management.commands.smash_stuff import Command as SmashStuff
+        self.ta1.philter = ''
+        self.ta1.save()
+        
+        self.ta1.feeds.add(self.feed1)
+        self.ta1.feeds.add(self.feed2)
+        c = SmashStuff()
+        # Run the smash_stuff command
+        results = c.handle(dryrun=True, quiet=True)
+        self.assertEquals(results['accounts_ready'], 2)
+        self.assertEquals(results['accounts_skipped'], 0)
+        self.assertEquals(results['feeds_pulled'], 2)
+        self.assertEquals(results['feeds_checked'], 2)
+        self.assertEquals(results['messages_added'], 40)
+        self.assertTrue(results['messages_added'] == results['entries_tweeted'] == results['entries_pulled'])
+        
+    def testSmashStuffCommandFilter(self):
+        "Sets up two feeds to get smashed into another and runs through the smash_stuff command with filter"
+        from twittersmash.management.commands.smash_stuff import Command as SmashStuff
+        self.ta1.philter = 'tinyurl'
+        self.ta1.save()
+        
+        self.ta1.feeds.add(self.feed1)
+        self.ta1.feeds.add(self.feed2)
+        c = SmashStuff()
+        # Run the smash_stuff command
+        
+        results = c.handle(dryrun=True, quiet=True)
+        self.assertEquals(results['accounts_ready'], 2)
+        self.assertEquals(results['accounts_skipped'], 0)
+        self.assertEquals(results['feeds_pulled'], 2)
+        self.assertEquals(results['feeds_checked'], 2)
+        self.assertEquals(results['messages_added'], 40)
+        self.assertTrue(results['messages_added'] == results['entries_pulled'])
+        self.assertTrue(results['messages_added'] > results['entries_tweeted'])
+
+    def testSmashStuffCommandManyFilters(self):
+        "Sets up two feeds to get smashed into another and runs through the smash_stuff command with multiple filters"
+        from twittersmash.management.commands.smash_stuff import Command as SmashStuff
+        self.ta1.philter = 'palin, bush, obama'
+        self.ta1.save()
+        
+        self.ta1.feeds.add(self.feed1)
+        self.ta1.feeds.add(self.feed2)
+        c = SmashStuff()
+        # Run the smash_stuff command
+        results = c.handle(dryrun=True, quiet=True)
+        self.assertEquals(results['accounts_ready'], 2)
+        self.assertEquals(results['accounts_skipped'], 0)
+        self.assertEquals(results['feeds_pulled'], 2)
+        self.assertEquals(results['feeds_checked'], 2)
+        self.assertEquals(results['messages_added'], 40)
+        self.assertTrue(results['messages_added'] == results['entries_pulled'])
+        self.assertTrue(results['messages_added'] > results['entries_tweeted'])
+
+    def testSmashStuffCommandOneRateLimit(self):
+        "Sets up two feeds to get smashed into another and runs through the smash_stuff command.  One account doesn't need to be checked."
+        from twittersmash.management.commands.smash_stuff import Command as SmashStuff
+        self.ta1.philter = ''
+        self.ta1.save()
+        
+        self.feed1.polling_rate = 5
+        self.feed1.last_checked = datetime.datetime.now()
+        self.feed1.save()
+        
+        self.ta1.feeds.add(self.feed1)
+        self.ta1.feeds.add(self.feed2)
+        c = SmashStuff()
+        # Run the smash_stuff command
+        results = c.handle(dryrun=True, quiet=True)
+        self.assertEquals(results['accounts_ready'], 1)
+        self.assertEquals(results['accounts_skipped'], 1)
+        self.assertEquals(results['feeds_checked'], 2)
+        self.assertEquals(results['feeds_pulled'], 1)
+        self.assertEquals(results['messages_added'], 20)
+        self.assertTrue(results['messages_added'] == results['entries_pulled'] == results['entries_tweeted'])
+    
+    def testSmashStuffCommandTwoRateLimit(self):
+        "Sets up two feeds to get smashed into another and runs through the smash_stuff command.  No accounts need to be checked."
+        from twittersmash.management.commands.smash_stuff import Command as SmashStuff
+        self.ta1.philter = ''
+        self.ta1.save()
+
+        self.feed1.polling_rate = 5
+        self.feed1.last_checked = datetime.datetime.now()
+        self.feed1.save()
+        
+        self.feed2.polling_rate = 5
+        self.feed2.last_checked = datetime.datetime.now()
+        self.feed2.save()
+
+        self.ta1.feeds.add(self.feed1)
+        self.ta1.feeds.add(self.feed2)
+        c = SmashStuff()
+        # Run the smash_stuff command
+        results = c.handle(dryrun=True, quiet=True)
+        self.assertEquals(results['accounts_ready'], 0)
+        self.assertEquals(results['accounts_skipped'], 2)
+        self.assertEquals(results['feeds_checked'], 2)
+        self.assertEquals(results['feeds_pulled'], 0)
+        self.assertEquals(results['messages_added'], 0)
+        self.assertTrue(results['messages_added'] == results['entries_pulled'] == results['entries_tweeted'])
+        
+    def testSmashStuffCommandRateLimitKicksIn(self):
+        "Sets up two feeds to get smashed into another and runs through the smash_stuff command.  Intitially neither account needs checked, but then one does."
+        from twittersmash.management.commands.smash_stuff import Command as SmashStuff
+        self.ta1.philter = ''
+        self.ta1.save()
+
+        self.feed1.polling_rate = 1
+        self.feed1.last_checked = datetime.datetime.now() - datetime.timedelta(seconds=50)
+        self.feed1.save()
+
+        self.feed2.polling_rate = 1
+        self.feed2.last_checked = datetime.datetime.now()
+        self.feed2.save()
+
+        self.ta1.feeds.add(self.feed1)
+        self.ta1.feeds.add(self.feed2)
+        c = SmashStuff()
+        # Run the smash_stuff command
+        results = c.handle(dryrun=True, quiet=True)
+        self.assertEquals(results['accounts_ready'], 0)
+        self.assertEquals(results['accounts_skipped'], 2)
+        self.assertEquals(results['feeds_checked'], 2)
+        self.assertEquals(results['feeds_pulled'], 0)
+        self.assertEquals(results['messages_added'], 0)
+        self.assertTrue(results['messages_added'] == results['entries_pulled'] == results['entries_tweeted'])
+        
+        time.sleep(10)
+        
+        c = SmashStuff()
+        # Run the smash_stuff command
+        results = c.handle(dryrun=True, quiet=True)
+        self.assertEquals(results['accounts_ready'], 1)
+        self.assertEquals(results['accounts_skipped'], 1)
+        self.assertEquals(results['feeds_checked'], 2)
+        self.assertEquals(results['feeds_pulled'], 1)
+        self.assertEquals(results['messages_added'], 20)
+        self.assertTrue(results['messages_added'] == results['entries_pulled'] == results['entries_tweeted'])
+        
+    
